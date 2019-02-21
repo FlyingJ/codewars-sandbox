a4b7798ce79dfbf69d495b2b4a5e3c371a9bd6ee
J Harrington <jason.m.harrington@gmail.com>
2019-02-21 13:49:18 -0500

    Add class for fibonacci product sequence, write productFib solution

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -28,9 +28,6 @@ class Cache():
         '''
         self.cached_items.append(value)
 
-#def initialize_fibonacci_cache():
-#    return Cache([0, 1])
-
 class Fibonacci():
     '''
     a class for fibonacci sequence objects
@@ -54,17 +51,42 @@ class Fibonacci():
     def __repr__(self):
         return str(self.cache)
 
-##def fibonacci_product(n):
-##    try:
-##        # return cached value
-##        return fibonacci_product_cache[n]
-##    except IndexError:
-#        new_value = fibonacci(n) * fibonacci(n+1)
-##        update_cache(fibonacci_product_cache, new_value)
-#        return new_value
-##    except TypeError:
-##        # sequence term identifier must be an integer
-##        return None
+class FibonacciProduct():
+    '''
+    a class for sequences of products of consecutive Fibonacci sequence
+    terms
+    '''
+    def __init__(self):
+        '''
+        create:
+        - cache object to speed generation of sequence elements
+        - Fibonacci sequence object
+        '''
+        self.cache = Cache([])
+        self.FIB = Fibonacci()
+
+    def __call__(self, term):
+        return self.FIB(term) * self.FIB(term+1)
+
+    def inf_scan(self, target_product):
+        '''
+        scan Fibonacci product sequence for smallest term equal to, or
+        greater than, product
+        return index of term satisfying condition and a boolean value
+        indicating equality
+        '''
+        term = 0
+        while True:
+            FIB_PRODUCT = self(term)
+            if FIB_PRODUCT >= target_product:
+                return [term, FIB_PRODUCT == target_product]
+            term += 1
+
+def productFib(target_product):
+    FIB = Fibonacci()
+    FIB_PROD = FibonacciProduct()
+    term, equality = FIB_PROD.inf_scan(target_product)
+    return [FIB(term), FIB(term+1), equality]
 
 if __name__ == '__main__':
 
@@ -76,15 +98,14 @@ if __name__ == '__main__':
     print(FIB)
     print(FIB(30))
     print(FIB)
-#    print(fibonacci(0))
-##    print(fibonacci_cache)
-#    print(fibonacci(5))
-##    print(fibonacci_cache)
-#
-#    print(fibonacci(10))
-#    print(fibonacci(11))
-#    print(fibonacci_product(10))
-##    print(fibonacci_product_cache)
-##    print(fibonacci_cache)
+
+    FIB_PROD = FibonacciProduct()
+    print(FIB_PROD(0))
+    print(FIB_PROD(1))
+    print(FIB_PROD(2))
+    print(FIB_PROD.inf_scan(5))
+
 #    test.assert_equals(productFib(4895), [55, 89, True])
 #    test.assert_equals(productFib(5895), [89, 144, False])
+    print(productFib(4895) == [55, 89, True])
+    print(productFib(5895) == [89, 144, False])
